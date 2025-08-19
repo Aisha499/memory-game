b175207c02119d41636a691c928bbd92bb20d360
Aisha Haji <hajywwsh@gmail.com>
2025-08-19 17:36:18 +0300

    planning phase -third- commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
-/*-------------------------------- Constants --------------------------------*/
+ /*-------------------------------- Constants --------------------------------*/
  
 /*---------------------------- Variables (state) ----------------------------*/
 let Firstchoice=null;
-let scendchoice=null;
-let setTime=0;
-
+let secendchoice=null;
+let countTry=0;
+let countwin=0;
+let countlose=0;
+ let wincard=null;
+ let losecard=null;
 
 /*------------------------ Cached Element References ------------------------*/
  const Display= document.querySelector("#result-display");
@@ -12,53 +15,79 @@ let setTime=0;
  let card2=document.querySelector("#card2");
  let card3=document.querySelector("#card3");
  let card4=document.querySelector("#card4");
- let time=document.querySelector("#setTimer");
- const restart=document.querySelector("#restart");
-
-/*-------------------------------- Functions --------------------------------*/
+ let card5=document.querySelector("#card5");
+ let card6=document.querySelector("#card6");
+ let ctry=document.querySelector("#setTimer");
+ let parent = document.querySelector("#button-wrapper");
+ /*-------------------------------- Functions --------------------------------*/
   
 const plays=(event) =>{
     if(Firstchoice ===null){
     Firstchoice=event.target.dataset.value;
+    }else if( secendchoice===null){
+     secendchoice=event.target.dataset.value;
     }
-    else {
-        if(Firstchoice===event.target.dataset.value){
-            return;
+ };
+ function compare(event){
+      const emoji =event.target.querySelector(".hidden");
+    if(Firstchoice=== secendchoice){
+         countwin+=1;
+         wincard=[Firstchoice,secendchoice];
+         compare2();
+         Firstchoice=null;
+         secendchoice=null;
+    } else if(Firstchoice !==secendchoice){
+         countlose+=1;
+          wincard=[Firstchoice,secendchoice];
+           compare2();
+         Firstchoice=null;
+         secendchoice=null;
+     } 
+     
+    };
+
+    function compare2(){
+        if(countwin>=3){
+         window.location.href="win.html";
+        }else if(countlose>=2){
+         window.location.href="lose.html";
         }
-    scendchoice=event.target.dataset.value;
     }
-
-};
-const compare =() =>{
-
-    if((Firstchoice==='card1' && scendchoice==='card3' )|| (Firstchoice==='card2' && scendchoice==='card4') || (Firstchoice==='card5' && scendchoice==='card6') || (Firstchoice==='card3' && scendchoice==='card1' )|| (Firstchoice==='card4' && scendchoice==='card2') || (Firstchoice==='card6' && scendchoice==='card5') ){
-        window.location.href="win.html";
-    } else {
-        window.location.href="lose.html";
-     }};
-
 
  
 const start =(event)=>{
-    plays(event);
-    if(Firstchoice&&scendchoice){
-    compare();
-     }
-     setInterval(timer,1000);
-     
+  const emoji =event.target.querySelector(".hidden");
+  if(emoji){
+    emoji.classList.remove("hidden");
+  }
+     plays(event);
+    if(Firstchoice&& secendchoice){
+      setTimeout(compare,300);
 }
  
+};
 const playAgain =()=>{
     Firstchoice=null;
-    scendchoice=null;
-    window.location.href="index.html";
+     secendchoice=null;
+     countwin=0;
+     countlose=0;
+     mix();
+}
+function mix() {
+   let arr = [card1, card2, card3, card4, card5, card6];
+
+  for (let i = arr.length - 1; i > 0; i--) {
+    const k = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[k]] = [arr[k], arr[i]];
+  }
+  parent.innerHTML = "";
+  arr.forEach(card => parent.appendChild(card));
 }
 
-function timer(){
-setTime +=Math.floor(Math.random()*3);
-time.textContent=setTime;
+window.onload = () => {
+  mix();
+};
 
-}
  
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -71,4 +100,3 @@ time.textContent=setTime;
  document.querySelector("#card6").addEventListener('click',  start);
  document.querySelector("#restart").addEventListener('click',playAgain);
  
-
