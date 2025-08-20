7451db2d034f72cbb5cb2481ac3f452122c2c89f
Aisha Haji <hajywwsh@gmail.com>
2025-08-20 12:49:42 +0300

    planning phase -first commit

--- js/app.js
+++ js/app.js
@@ -1,49 +1,35 @@
  /*-------------------------------- Constants --------------------------------*/
  
 /*---------------------------- Variables (state) ----------------------------*/
-let Firstchoice=null;
-let secendchoice=null;
-let countTry=0;
-let countwin=0;
+ let countwin=0;
 let countlose=0;
- let wincard=null;
- let losecard=null;
+ let cardf=null;
+ let cards=null;
 
 /*------------------------ Cached Element References ------------------------*/
- const Display= document.querySelector("#result-display");
- let card1=document.querySelector("#card1");
- let card2=document.querySelector("#card2");
- let card3=document.querySelector("#card3");
- let card4=document.querySelector("#card4");
- let card5=document.querySelector("#card5");
- let card6=document.querySelector("#card6");
- let ctry=document.querySelector("#setTimer");
- let parent = document.querySelector("#button-wrapper");
+  let count=document.querySelector("#countplay");
+  let parent = document.querySelector("#button-wrapper");
  /*-------------------------------- Functions --------------------------------*/
-  
-const plays=(event) =>{
-    if(Firstchoice ===null){
-    Firstchoice=event.target.dataset.value;
-    }else if( secendchoice===null){
-     secendchoice=event.target.dataset.value;
-    }
- };
- function compare(event){
-      const emoji =event.target.querySelector(".hidden");
+ function compare(){
+    const Firstchoice=cardf.dataset.value;
+    const secendchoice=cards.dataset.value;
+
     if(Firstchoice=== secendchoice){
-         countwin+=1;
-         wincard=[Firstchoice,secendchoice];
+         countwin+=1; 
          compare2();
-         Firstchoice=null;
-         secendchoice=null;
-    } else if(Firstchoice !==secendchoice){
+          cardf=null;
+          cards=null;
+    } else{
+        cardf.querySelector("div").classList.add("hidden");
+        cards.querySelector("div").classList.add("hidden");
          countlose+=1;
-          wincard=[Firstchoice,secendchoice];
-           compare2();
-         Firstchoice=null;
-         secendchoice=null;
+          compare2();
+        cardf=null;
+        cards=null;
+         
      } 
-     
+     count.textContent=`win: ${countwin} | lose ${countlose}`;
+         
     };
 
     function compare2(){
@@ -55,24 +41,23 @@ const plays=(event) =>{
     }
 
  
-const start =(event)=>{
-  const emoji =event.target.querySelector(".hidden");
+function start(event){
+    const card =event.currentTarget;
+  const emoji =card.querySelector(".hidden");
   if(emoji){
     emoji.classList.remove("hidden");
   }
-     plays(event);
-    if(Firstchoice&& secendchoice){
-      setTimeout(compare,300);
-}
- 
+  if(!cardf){
+    cardf=card;
+  }else if(!cards && card !==cardf){
+    cards=card;
+    setTimeout(compare,800);
+  }
+   
 };
-const playAgain =()=>{
-    Firstchoice=null;
-     secendchoice=null;
-     countwin=0;
-     countlose=0;
+
      mix();
-}
+     
 function mix() {
    let arr = [card1, card2, card3, card4, card5, card6];
 
@@ -92,11 +77,6 @@ window.onload = () => {
 
 /*----------------------------- Event Listeners -----------------------------*/
  
- document.querySelector("#card1").addEventListener('click', start);
- document.querySelector("#card2").addEventListener('click',  start);
- document.querySelector("#card3").addEventListener('click',  start);
- document.querySelector("#card4").addEventListener('click',  start);
- document.querySelector("#card5").addEventListener('click',  start);
- document.querySelector("#card6").addEventListener('click',  start);
- document.querySelector("#restart").addEventListener('click',playAgain);
- 
+document.querySelectorAll("button").forEach(card=>{
+    card.addEventListener("click",start)
+});
